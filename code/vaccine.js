0cc687cf308bf5884ce0d6dc501ec9da838d6f7c
abia456 <l174192@lhr.nu.edu.pk>
2021-06-21 01:15:17 +0500

    abia6

--- code/vaccine.js
+++ code/vaccine.js
@@ -6,19 +6,19 @@ const html = require('html');
 var mysql = require('mysql2');
 
 
-/*var con = mysql.createConnection({
+var con = mysql.createConnection({
   host: "localhost",
   user: "abianoor456",
   password: "abiasql123456",
   database: 'cloudproject'
-});*/
+});
 
-var con = mysql.createConnection({
+/*var con = mysql.createConnection({
   username: process.env.DB_USERNAME || 'abianoor456',
   password: process.env.DB_PASSWORD || 'abiasql123456',
   host: process.env.DB_HOST || 'localhost',
   database: 'cloudproject'
-});
+});*/
 
 con.connect(function(err) {
   if (err) throw err;
@@ -49,6 +49,34 @@ router.get('/ViewVacApp', function(req,res){
 	res.sendFile(path.join(__dirname+'/ViewVacApp.html'));
 });
 
+//	Req Appointment
+router.post('/ReqPost', function(req,res){
+	
+	console.log('ReqPost');
+	console.log(req.body.user_name);
+	console.log(req.body.user_age);
+
+	//var sql = "INSERT INTO users (userName, userAge) VALUES (" + req.user_name +',' + req.user_age + ")";
+	
+	var sql = "INSERT INTO users(userID,userName,userAge) VALUES (?,?,?)";
+  con.query(sql, [11,req.body.user_name, req.body.user_age], function (err, result) {
+    if (err) throw err;
+	user_id = result.insertId;
+    console.log("1 record inserted, ID: " + result.insertId);
+	console.log("Addimg into appintments table");
+	
+  var sql2 = "INSERT INTO vaccine_appointment (userID, centerID, dose_number) VALUES ( ?,?,?)";
+    con.query(sql2, [user_id ,  req.body.vaccination_centre , 1], function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted, ID: " + result.insertId);
+	res.sendFile(path.join(__dirname+'/index.html'));
+	
+
+	});
+
+	//res.sendFile(path.join(__dirname+'/ReqApp.html'));
+});
+});
 
 
 router.post('/ViewVacApp', function (req, res) {
