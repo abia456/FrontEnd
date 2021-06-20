6b9975f588c18b75b1b3076f3b3577ea00dc3a70
amna-akram <amna.akram185@gmail.com>
2021-06-20 21:20:02 +0500

    commit snek2

--- code/vaccine.js
+++ code/vaccine.js
@@ -3,6 +3,25 @@ const router =  express.Router();
 const path = require('path');
 const url = require('url');
 const html = require('html');
+var mysql = require('mysql2');
+
+var con = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "amnabhatti",
+  database: 'cloudproject'
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+con.query('select * from users', function(err, result, fields) {
+    if (err) {
+        return console.log(err);
+    }
+    return console.log(result);
+});
 
 //	register
 router.get('/', function(req,res){
