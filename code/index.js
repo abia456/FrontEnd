1020c7bbd995d9088948d8d7f20d0ce1d7a28b65
hamza-murad <hamzamurad1@hotmail.com>
2021-06-21 10:03:15 +0500

    fix public and view paths

--- code/index.js
+++ code/index.js
@@ -6,19 +6,18 @@ const path = require('path');
 const app = express();
 app.set('view engine', 'ejs');
 const http = require("http").createServer(app);
-
 var bodyParser = require("body-parser");
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 //Static folder, public foldeer
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'public')));
 
 
 let vaccine = require('./vaccine');
 app.use('/',vaccine);
-
+app.set('views', path.join(__dirname, '/views'));
 
 
 http.listen(8000, function(){
